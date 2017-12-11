84fbed24b4e8ee9073e79e01947addcdf90423d7
Ernesto Pariona <eparionad@gmail.com>
2017-12-11 18:09:51 -0500

    se modifico el archivo settings.py para trabajar en ambientes de desarrollo y producción

---
+++ reconocimientoVoz/reconocimientoVoz/settings/local.py
@@ -0,0 +1,15 @@
+from .base import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'reconocimientovoz',
+        'USER': 'postgres',
+        'PASSWORD': 'ernesto123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+STATIC_URL = '/static/'
